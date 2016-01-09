edaeb38f846990f49a9e30ad61b8b2335db49779
rdevon <devon@cs.unm.edu>
2016-01-09 11:40:48 -0700

    fixed script

--- script_1.py
+++ script_1.py
@@ -1,14 +1,16 @@
 import argparse
 parser = argparse.ArgumentParser(description='number_from_user')
 parser.add_argument('inp', type=float)
-args=parser.parse_args()
-print(args.inp*10+8)
+args = parser.parse_args()
+print(args.inp * 10 + 8)
 
 import theano.tensor as T
 from theano import function
-x=T.dscalar('x')
-y=T.dscalar('y')
+x = T.dscalar('x')
+#y = T.dscalar('y') * Don't need this
 # Type is not matching, namaspace type to theano tensorType
-x=args.inp
-y=10*x+8
-f=function([],y)
+#x = args.inp ** you reassigned x here.
+y = 10 * x + 8
+f = function([x], y)
+
+print f(args.inp)
